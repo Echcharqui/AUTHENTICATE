80ff477fdbc2d6d0f9c967a14a3d73afa6ce8b14
echcharqui <echcharqui@gmail.com>
2022-02-16 19:45:04 +0100

    chages on the msg feedback on auth routes

--- src/routes/auth/auth_router.js
+++ src/routes/auth/auth_router.js
@@ -3,22 +3,22 @@ const router = require('express').Router()
 
 // sign in route
 router.get('/signIn', async (req, res) => {
-  return res.status(200).json({ message: 'sign in !' })
+  return res.status(200).json({ message: 'welcom to the sign in !' })
 })
 
 // sign up route
 router.get('/signUp', async (req, res) => {
-  return res.status(200).json({ message: 'sign up !' })
+  return res.status(200).json({ message: 'welcom to the sign up !' })
 })
 
 // forgetPassword route
 router.get('/forgetPassword', async (req, res) => {
-  return res.status(200).json({ message: 'forgetPassword !' })
+  return res.status(200).json({ message: 'welcom to the forgetPassword !' })
 })
 
 // log out route
 router.get('/logOut', async (req, res) => {
-  return res.status(200).json({ message: 'log out !' })
+  return res.status(200).json({ message: 'welcom to the log out !' })
 })
 
 module.exports = router
